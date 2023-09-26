45f50ed8525a49b13ee2887b0232f598a532e2c6
Pinu Maharjan <pinumaharjan49@gmail.com>
2023-09-26 07:47:33 -0700

    List the items

---
+++ src/List.jsx
@@ -0,0 +1,11 @@
+function List(props){
+    const{List} = props
+    return(
+        <ul>
+            <li>List of fruits</li>
+            <li>List of veggies</li>
+            <li>List of juice</li>
+        </ul>
+    )
+}
+export default List;
