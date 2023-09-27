44ea993ad24283a4b2a975b011f6bacfd65952ad
Pinu Maharjan <pinumaharjan49@gmail.com>
2023-09-27 07:11:05 -0700

    New branch created

--- src/List.jsx
+++ src/List.jsx
@@ -5,7 +5,11 @@ function List(props){
             <li>List of fruits</li>
             <li>List of veggies</li>
             <li>List of juice</li>
+            <p>This is a new list</p>
         </ul>
+        
+
     )
 }
 export default List;
+
